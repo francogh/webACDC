635f701d323e02a68a7d77a4449ef316c58e5c89
franco ghiotti <francoghiotti@gmail.com>
2021-07-16 12:30:08 -0300

    nada, solo musica

--- js/reprod.js
+++ js/reprod.js
@@ -12,3 +12,42 @@ playPause.addEventListener("click", () => {
         playPause.querySelector('.play-btn').classList.toggle('hide');
     }
 });
+
+// let previus = document.querySelector('#back');
+// let next = document.querySelector('#next');
+// let track_image = document.querySelector('#back');
+
+
+
+
+// let track = document.createElement('song');
+
+// let index_no = 0;
+// let playing_song = false;
+
+// let All_song = [
+//     {
+//         name: "ThundersTruck",
+//         path: "./media/song.mp3",
+
+
+//     },
+//     {
+//         name: "Back in Black",
+//         path: "./media/song.mp3",
+
+
+//     },
+//     {
+//         name: "Highway to Hell",
+//         path: "./media/song.mp3",
+
+
+//     }
+// ];
+
+// function load_track(index_no){
+//     track.src = All_song[index_no].path;
+//     track.load();
+// }
+// load_track(index_no);
